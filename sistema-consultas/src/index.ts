a890a96b8209d577a25866f223e211be511783f3
JonasSantoos <jscom3121@gmail.com>
2026-02-25 20:54:04 -0300

    feat: Dados base e consulta criados

--- sistema-consultas/src/index.ts
+++ sistema-consultas/src/index.ts
@@ -1,6 +1,42 @@
-// import { Especialidade } from "./types/especialidade";
-// import { Paciente } from "./types/paciente";
-// import { StatusConsulta } from "./types/statusConsulta";
-// import { Medico } from "./interfaces/medico";
-// import { Consulta } from "./interfaces/consulta";
+import { Paciente } from "./types/paciente";
+import { StatusConsulta } from "./types/statusConsulta";
+import { Medico } from "./interfaces/medico";
+import { Consulta } from "./interfaces/consulta";
+import { Especialidade } from "./types/especialidades";
+
+
+const cardiologia: Especialidade = {
+  id: 1,
+  nome: "Cardiologia",
+};
+const medico1: Medico = {
+  id: 1,
+  nome: "Dr. Roberto Silva",
+  crm: "CRM12345",
+  especialidade: cardiologia,
+  ativo: true,
+};
+const paciente1: Paciente = {
+  id: 1,
+  nome: "Carlos Andrade",
+  cpf: "123.456.789-00",
+  email: "carlos@email.com",
+};
+
+function criarConsulta(
+  id: number,
+  medico: Medico,
+  paciente: Paciente,
+  data: Date,
+  valor: number
+): Consulta {
+  return {
+    id,
+    medico,
+    paciente,
+    data,
+    valor,
+    status: "agendada",
+  };
+}
 
